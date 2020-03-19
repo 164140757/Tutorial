0c2d701d42eeb487c35dbd1ab25e0588a4168169
164140757@qq.com <164140757@qq.com>
2020-03-19 11:24:56 +0800

    new

--- src/main/java/MultiThreads/SimpleThread.java
+++ src/main/java/MultiThreads/SimpleThread.java
@@ -10,12 +10,29 @@ public class SimpleThread implements Runnable {
     // entry point where the thread will enter when start() is called
     public void run() {
         try {
+            // number is used to count down
             for(int i = 5; i>0;i++){
                 System.out.println("Child Thread: "+i);
                 Thread.sleep(500);
             }
         } catch (InterruptedException e) {
-            e.printStackTrace();
+            System.out.println("Child interrupted.");
         }
+        System.out.println("Existing Child thread.");
+    }
+
+    public static void main(String[] args) {
+        SimpleThread st = new SimpleThread();
+        // start child thread, run() will be called
+        st.t.start();
+        try {
+            for(int i = 5; i>0;i++){
+                System.out.println("Main Thread: "+i);
+                Thread.sleep(500);
+            }
+        } catch (InterruptedException e) {
+            System.out.println("Main thread interrupted.");
+        }
+        System.out.println("Existing Main thread.");
     }
 }
